c96cf104800205f7ffb1585f8541cd3946b52de9
Sachin <vs1sachi12@gmail.com>
2025-04-25 14:32:04 +0530

    added files12345

---
+++ models/user-model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const bcryptjs = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "password is required"],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+userSchema.pre("save", async function (next) {
+  const salt = await bcryptjs.genSalt(10);
+  this.password = await bcryptjs.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.generateToken = function () {
+  return jwt.sign({ id: this._id }, "x-auth-token", {
+    expiresIn: "3d",
+  });
+};
+
+userSchema.methods.verifypass = async function (userEnteredPass) {
+  return bcryptjs.compare(userEnteredPass, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
--- server.js
+++ server.js
@@ -1,18 +1,52 @@
-const express = require("express")
-const cors = require("cors")
+const express = require("express");
+const mongoose = require("mongoose");
+const User = require("./models/user-model");
 
-const app = express()
-app.use(express.json())
-app.use(express.urlencoded({extended:false}))
-app.use(cors({
-    origin : "*",
-    methods : ["GET","POST","PUT","PATCH","DELETE"]
-}))
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
-app.get("/",(req,res)=>{
-    res.status(200).json({success:true,message:"Hello sarayu infotech"})
-})
+app.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await User.create({ name, email, password });
+    const token = await user.generateToken();
+    res.status(201).json({ success: true, token });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
 
-app.listen(5000,()=>{
-    console.log("Listening on port 5000");
-})
+app.post("/signin", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Email not registered" });
+    }
+    const validate = await user.verifypass(password);
+    if (!validate) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Invalid password" });
+    }
+    const token = await user.generateToken();
+    res.status(200).json({ success: true, token });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+});
+
+mongoose
+  .connect("mongodb://localhost:27017/sachin")
+  .then(() => {
+    console.log("Database connection successful!");
+    app.listen(5000, () => {
+      console.log("Listening on port number 5000");
+    });
+  })
+  .catch(() => {
+    console.log("Database connection failed!");
+  });
